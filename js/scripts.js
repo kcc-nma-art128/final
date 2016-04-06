74b7569615f8d1fddc5da95f788117a33325f70e
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-04-06 00:25:48 -1000

    Try sticky nav-Fail

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,15 @@ $(function() {
     }
   });
 });
+
+var  mn = $(".mainnav");
+    mns = "mainnav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
