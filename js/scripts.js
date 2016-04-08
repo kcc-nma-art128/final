6ac438f64d7278e242a41d394b5e28d36d9a7f10
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-04-08 00:23:59 -1000

    +Home's

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//////////////////////////////////////////////NAV
 $(function() {
   var $navToggle = $('.nav-toggle');
   var $mainNavUl = $('.mainnav li');
@@ -16,3 +17,35 @@ $(function() {
     }
   });
 });
+
+
+
+
+//////////////////////////////////////Slideshow
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].classList.add("active");
+}
