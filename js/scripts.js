e76be4a1b46c3801db51734fd39bdeb405d9dfef
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-04-14 16:16:24 -1000

    #gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 //////////////////////////////////////////////NAV
 $(function() {
   var $navToggle = $('.nav-toggle');
@@ -18,10 +19,7 @@ $(function() {
   });
 });
 
-
-
-
-//////////////////////////////////////Slideshow
+//////////////////////////////////////////////slide
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -48,4 +46,68 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].classList.add("active");
+}
+
+
+
+
+
+
+
+var slideIndex = 1;
+showDivs1(slideIndex);
+
+function plusDivs1(n) {
+    showDivs1(slideIndex += n);
+}
+
+function showDivs1(n) {
+    var i;
+    var x = document.getElementsByClassName("g-slide1");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+    showDivs2(slideIndex += n);
+}
+
+function showDivs2(n) {
+    var i;
+    var x = document.getElementsByClassName("g-slide2");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+
+
+var slideIndex = 1;
+showDivs3(slideIndex);
+
+function plusDivs3(n) {
+    showDivs3(slideIndex += n);
+}
+
+function showDivs3(n) {
+    var i;
+    var x = document.getElementsByClassName("g-slide3");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
 }
