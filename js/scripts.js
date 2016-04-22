e18bfbf8b22ef95699ebe98eab8c346916383de1
SkyNhiNguyen <nnguyen9@hawaii.edu>
2016-04-21 17:27:46 -1000

    +liquids

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,6 @@
 
 
 
-//////////////////////////////////////////////NAV
-$(function() {
-  var $navToggle = $('.nav-toggle');
-  var $mainNavUl = $('.mainnav li');
-  var $window = $(window);
-  var mobileBreakpoint = 768;
-
-  $navToggle.on('click', function() {
-    $mainNavUl.slideToggle();
-    $navToggle.toggleClass('active');
-  });
-
-  $window.on('resize', function() {
-    if ($window.width() < mobileBreakpoint & $mainNavUl.is(':hidden')) {
-      $mainNavUl.removeAttr('style');
-      $navToggle.removeClass('active');
-    }
-  });
-});
 
 //////////////////////////////////////////////slide
 
