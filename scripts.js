db9bf0e59e54a44584b24af29593a077421e4175
alissamas <nmastudent@KOPMAC004.local>
2016-05-03 09:58:26 -1000

    styled input box

--- scripts.js
+++ scripts.js
@@ -24,13 +24,14 @@ $faqToggle.on('click', function() {
 
 
 
-
 $('.slider').slick({
   dots: true,
   infinite: false,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 3,
+  autoplay: true,
+  autoplaySpeed: 2000,
   responsive: [
     {
       breakpoint: 1024,
